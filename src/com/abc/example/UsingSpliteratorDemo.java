579c83e30d4ce73945c8efe3fe4d393341e1a5e5
Arti123750 <bombalearti@gmail.com>
2020-01-13 12:20:18 +0530

    added one more prog

--- src/com/abc/example/UsingSpliteratorDemo.java
+++ src/com/abc/example/UsingSpliteratorDemo.java
@@ -1,10 +1,30 @@
 package com.abc.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
 public class UsingSpliteratorDemo {
 
 	public static void main(String[] args)
 {
 
+		List<Integer>l1=new ArrayList<Integer>();
+		l1.add(78);
+		l1.add(97);
+		l1.add(731);
+		l1.add(9713);
+		l1.add(71);
 		
+		Spliterator<Integer>sp=l1.spliterator();
+		
+		for (Integer inte: l1) {
+			
+			System.out.println("Elements: "+inte);
+			
+		}
+		System.out.println(sp.estimateSize());
+		System.out.println(sp.characteristics());
 		
 }
 }
